abb172d7517fc0ec3d990e0515ae6eb64ddab899
Ivan Topal <topal24@yandex.ru>
2022-03-26 17:31:45 +0300

    Include config.py to .gitignore

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-from python_org_news import get_python_news
-from weather import weather_by_city
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
 
 
 def create_app():
@@ -16,6 +16,8 @@ def create_app():
         return render_template('index.html', page_title=title, weather=weather, news_list=news)
 
     return app
+
+
 
 
 if __name__ == "__main__":
