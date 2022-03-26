047adee32e281ab7be89d148c607b6f40aecfb0e
Ivan Topal <topal24@yandex.ru>
2022-03-26 14:21:38 +0300

    Add .idea to the .gitignore file

---
+++ webapp/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+from python_org_news import get_python_news
+from weather import weather_by_city
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    @app.route('/')
+    def index():
+        title = "Новости Python"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news = get_python_news()
+        return render_template('index.html', page_title=title, weather=weather, news_list=news)
+
+    return app
+
+
+if __name__ == "__main__":
+    create_app().run(debug=True, port=50000)
+
---
+++ webapp/config.py
@@ -0,0 +1,3 @@
+WEATHER_DEFAULT_CITY = "Moscow, Russia"
+WEATHER_API_KEY = "33a2347b4d434a849e9144443221503"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
